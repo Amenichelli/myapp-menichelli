3e9b9dd76ac4014316686ceed2e51798831c1b21
Augusto Menichelli <augustosmenichelli@gmail.com>
2022-05-18 17:25:29 -0300

    entrega-11

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import DarkModeContextProvider from "./store/DarkModeContextProvider";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKaAoOe4E9RDfVpFnPpGjXrHEF6nUpSNA",
+  authDomain: "proyecto-menichelli.firebaseapp.com",
+  projectId: "proyecto-menichelli",
+  storageBucket: "proyecto-menichelli.appspot.com",
+  messagingSenderId: "128878545764",
+  appId: "1:128878545764:web:965bdf8e0a465a36864871",
+  measurementId: "G-NE9EK34H0D"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,81 +1,15 @@
 import React, { useContext,useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
 import ItemDetail from '../../components/ItemDetail/ItemDetail';
 import './ItemDetailContainer.css'
 
     function getItem(id) {
-            const myPromise = new Promise((resolve, reject) => {
-                    const productos = [
-                        {
-                            id: 1,
-                            nombre: 'Pizza Margherita',
-                            precio: '850',
-                            stock: "25",
-                            category:"pizza",
-                            descripcion: "Clásica pizza napoletana.",
-                            imagen: 'https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-                        },
-                        {
-                            id: 2,
-                            nombre: 'Fugazzeta Rellena',
-                            precio: '990',
-                            stock: "25",
-                            category:"pizza",
-                            descripcion: "Pizza rellena de queso y cubierta por cebollas ",
-                            imagen: 'https://www.rionegro.com.ar/wp-content/uploads/2021/04/174495648_1605928346274502_2986851770455894648_n.jpg'
-                        },
-                        {
-                            id: 3,
-                            nombre: 'Napo',
-                            precio: '950',
-                            stock: "25",
-                            category:"pizza",
-                            descripcion: "Clásica pizza napolitana argentina.",
-                            imagen: 'https://safariresto.com.ar/wp-content/uploads/2020/10/pizza-napolitana.jpg'
-                        },
-                        {
-                            id: 4,
-                            nombre: 'Cheeseburguer brioche',
-                            precio: '850',
-                            stock: "25",
-                            category:"burger",
-                            descripcion: "Burguer con pan bricoche, medallón de carne de 120grs, cheddar, lechuga, pepinillos y salsa especial.",
-                            imagen: 'https://images.pexels.com/photos/8879360/pexels-photo-8879360.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-                        },
-                        {
-                            id: 5,
-                            nombre: 'Cheeseburguer pan de papa',
-                            precio: '800',
-                            stock: "25",
-                            category:"burger",
-                            descripcion: "Burguer con pan bricoche, medallón de carne de 120grs, cheddar, lechuga, pepinillos y salsa especial.",
-                            imagen: 'https://images.pexels.com/photos/1556698/pexels-photo-1556698.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-                        },
-                        {
-                            id: 6,
-                            nombre: 'Brioche Bacon',
-                            precio: '900',
-                            stock: "25",
-                            category:"burger",
-                            descripcion: "Burguer con pan bricoche, medallón de carne de 120grs, cheddar, lechuga, pepinillos y salsa especial.",
-                            imagen: 'https://images.pexels.com/photos/2983099/pexels-photo-2983099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-                        },
-                        {
-                            id: 7,
-                            nombre: 'Brioche Blue',
-                            precio: '850',
-                            stock: "25",
-                            category:"burger",
-                            descripcion: "Burguer con pan bricoche, medallón de carne de 120grs, cheddar, lechuga, pepinillos y salsa especial.",
-                            imagen: 'https://media.istockphoto.com/photos/gourmet-burger-with-blue-cheese-picture-id483878830?k=20&m=483878830&s=612x612&w=0&h=5y4Vnfd_VSH1dUlqxbSzJssXFVtrBTgoe1hmFg6F93E='
-                        }
-                    ];
-                    const item = productos.filter(item => item.id === parseInt(id));
-                     setTimeout(() => {
-                                resolve(item[0])
-                            }, 2000);
-            });
-         return myPromise
+
+            const db = getFirestore();
+            const itemRef = doc(db, 'items', id);
+
+            return getDoc(itemRef);
         }  
         
     function ItemDetailContainer() {
@@ -84,12 +18,12 @@ import './ItemDetailContainer.css'
 
         useEffect (() =>{
          getItem( id )
-         .then( res=> {
-            setItem(res)
+         .then( snapshot => {
+            setItem({...snapshot.data(), id: snapshot.id})
         })
          .catch(err =>{
             console.log(err);
-            alert("Algo Falló :(")
+            alert("Algo Falló ")
         })
     }, [id]);
 
